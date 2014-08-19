78ef900914922391c0edb70fa1cb35acc0a21467
Alexander Le <alextle0125@gmail.com>
2014-08-18 21:45:21 -0400

    query 200 most recent soundcloud uploads based on datetime specification

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -4,7 +4,8 @@ end
 
 post '/genreoftheday' do
   @query_date = params[:date]
-  @latest_fifty = $client.get('/tracks', :created_at => {:to => @query_date})
+  @latest_fifty = $client.get('/tracks', :created_at => {:to => @query_date}, :limit => 200)
+  @latest_fifty = @latest_fifty.sort_by { |track| track[:playback_count] }.reverse
   erb :genreoftheday
 end
 
