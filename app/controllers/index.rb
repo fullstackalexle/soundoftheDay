404b8401cb1a82b8e2cde77fdf9818b527bbb1b8
Alexander Le <alextle0125@gmail.com>
2014-08-18 22:53:57 -0400

    ajaxified

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -6,7 +6,7 @@ post '/genreoftheday' do
   @query_date = params[:date]
   @latest_fifty = $client.get('/tracks', :created_at => {:to => @query_date}, :limit => 200)
   @latest_fifty = @latest_fifty.sort_by { |track| track[:playback_count] }.reverse
-  erb :genreoftheday
+  @latest_fifty.to_json
 end
 
 
